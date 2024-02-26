833b175d2a9c6a9c4d379a516c160ed3b822340c
Pau Julian <pau.julian@gmail.com>
2024-02-26 14:31:05 +0100

    fix full assert function file #195

--- staff/pau-julian/strings/assert.js
+++ staff/pau-julian/strings/assert.js
@@ -9,8 +9,30 @@ function hasValues(iterable) {
     console.assert(iterable.length === i, 'length ' + iterable.length + 'equals ' + count)
 }
 
+function equalsValue(value, target) {
+    console.assert(value === target, target)
+}
+
+function error(error, type, message) {
+    console.assert(error.name === type, 'name')
+    console.assert(error.message === message, 'message')
+}
+
+function valuesPropertiesMatch(iterable, compare, callback) {
+    for (var i = 0; i < compare.length; i++) {
+        var current = iterable[i]
+        var target = compare[i]
+
+        var matches = callback(current, target)
+
+        console.assert(matches, 'for index ' + i + '' + callback)
+    }
+}
+
+
 module.exports = {
-    assertFn: assertFn
-
-
+    hasValues: hasValues,
+    equalsValue: equalsValue,
+    error: error,
+    valuesPropertiesMatch: valuesPropertiesMatch,
 }
